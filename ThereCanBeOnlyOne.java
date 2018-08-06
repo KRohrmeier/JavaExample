f9b8e60461088833ae178cff436148160904bc3b
Kathrin Rohrmeier (Köhler) <theliteratecondition@gmail.com>
2018-08-05 22:43:18 -0500

    copy-edits

--- ThereCanBeOnlyOne.java
+++ ThereCanBeOnlyOne.java
@@ -6,22 +6,24 @@
 public class ThereCanBeOnlyOne {
 
     /** Placeholder for current(and only) singleton object.
-     *  Remember, static variable belongs to class, not to an instance.
+     *  Remember, static variables belongs to the class, not to an instance.
      *  Yes, this is weird. The class is holding an instance of itself as a
      *  variable. Kinda chicken-and-egg-y, except when you think about *when*
      *  static variables are created. Static is interesting magic.
      */
     private static ThereCanBeOnlyOne onlyOne = null;
 
-    /** Private constructor means no other class/object can instantiate.
-     * Another class/object wants the singleton? It can use the
-     * getInstance method below.
+    /** Private constructor means no other class/object can instantiate this
+     *  class. Another class/object wants to use the singleton? It can use 
+     *  the getInstance() method (further down).
      */
     private ThereCanBeOnlyOne() {
     }
 
     /** Public method for other classes/objects to ask for the singleton.
-     *  Static method is called on class itself - no need for an object instance!
+     *  Static methods are called on the class itself - no need for an 
+     *  object instance. Note the return type! Yup, the class of the 
+     *  return type is this class.
      */
     public static ThereCanBeOnlyOne getInstance() {
         if (onlyOne == null) {
@@ -31,11 +33,11 @@ public class ThereCanBeOnlyOne {
                         // or the one (and only) instance is returned
     }
 
-    /** Some method that only this class can do (why we might want a singleton) -
-     *  e.g. only the President can grant clemency for federal crimes.
-     *  There can be only one President, and only the President can pardon.
+    /** Some method that only this class can do, and why we might want a singleton
+     *  (e.g. only the President can grant clemency for federal crimes.
+     *  There can be only one President, and only the President can pardon).
      *  This is likely an important method, so we'll make it private and place
-     *  some logic needed to conduct this method in the class's methods...
+     *  some logic needed to conduct this method.   
      *  @param String, the request
      *  @param String, the person hoping to be pardoned
      */
@@ -49,7 +51,7 @@ public class ThereCanBeOnlyOne {
 
     /** Private method of the President singleton class - grants pardon
      *  @param String, the person hoping to be pardoned
-     *  @return decision to pardon
+     *  @return decision to grant pardon
      */
     private String grantPardon(String personToPardon) {
         String decision = "Madame President hereby pardons "
@@ -59,7 +61,7 @@ public class ThereCanBeOnlyOne {
 
     /** Private method of the President singleton class - denies pardon
      *  @param String, the person hoping to be pardoned
-     *  @return decision not to pardon
+     *  @return decision to deny pardon
      */
     private String denyPradon(String personToPardon) {
         String decision = "Madame President refuses to pardon "
