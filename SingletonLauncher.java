061b8b5e59fa53cd2288e665a66e351073a14cd2
Kathrin Rohrmeier (Köhler) <theliteratecondition@gmail.com>
2018-08-05 22:51:52 -0500

    Update SingletonLauncher.java

--- SingletonLauncher.java
+++ SingletonLauncher.java
@@ -1,12 +1,13 @@
 /**
- * singleton launcher class to practice using the static keyword by creating
- * a singleton class,
+ * The SingletonLauncher class drives the application. This class demonstrates
+ * the static keyword by creating a singleton class;
  * or, "what can you do with static and why would you want to?"
+ *
  * @author kkohler
  */
 public class SingletonLauncher {
 
-    /** Main method gets singleton demonstration going
+    /** Main method gets singleton demonstration going.
      *  Does NOT instantiate a singleton instance, because it can't! Instead,
      *  it calls the class's getInstance() method (which is static and
      *  available to be called on the class; no object instance needed!)
@@ -14,12 +15,14 @@ public class SingletonLauncher {
      */
     public static void main(String[] args) {
 
+        // Creating a variable we'll name "newPresident" and setting it equal
+        // to what the singleton's getInstance() method returns.
         ThereCanBeOnlyOne newPresident = ThereCanBeOnlyOne.getInstance();
         newPresident.requestPardon("Please grant clemency to this turkey.",
                 "the Thanksgiving Turkey");
 
-        // we can call methods without setting the singleton equal to a
-        // variable (there will only ever be the one anyway)
+        // We can call methods without setting the singleton equal to a
+        // variable (there will only ever be the one anyway).
         ThereCanBeOnlyOne.getInstance().requestPardon("Let this turkey go!",
                 "the Thanksgiving Turkey");
     }
